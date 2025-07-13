3dff943c0029792222adb5143cb931c391e95e13
sezryo <sezrienne@proton.me>
2025-07-13 05:30:43 +0100

    Init

--- depoly.sh
+++ depoly.sh
@@ -6,10 +6,11 @@ forester build forest.toml
 
 cd output
 
+git config user.signingkey 0xE1D631009F9086E1
 git add -A .
 git commit -m 'deploy'
 
-git remote add origin git@github.com:sezryo/yoyogi.git
+git remote add origin git@github.com:sezryo/sezryo.github.io.git
 git branch -M main
 git push -f origin main
 
