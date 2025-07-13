4a6dc387bbd6f4b38a1deb672ba4f13308b07a23
sezryo <sezrienne@proton.me>
2025-07-13 07:10:07 +0100

    commit

---
+++ init.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env sh
+
+set -e
+
+echo "use flake" >> .envrc
+echo "PATH_add bin" >> .envrc
+direnv allow # Install direnv or nix-direnv first
+
+git init
+git config user.signingkey 0xE1D631009F9086E1
+
+forester build forest.toml
+cd ./output/
+git init
+git config user.signingkey 0xE1D631009F9086E1
