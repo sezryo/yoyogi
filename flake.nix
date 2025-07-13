52bf8db94746ec35c2ec0a6e45344b3400765e53
sezryo <sezrienne@proton.me>
2025-07-13 04:59:04 +0100

    Init

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    forester.url = "sourcehut:~jonsterling/ocaml-forester";
+    forester.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, forester, nixpkgs }:
+    let
+      system = "x86_64-linux"; # make sure to change this to your use case!
+      pkgs = import nixpkgs { inherit system inputs; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [ forester.packages.${system}.default ];
+      };
+    };
+}
